9d1ee05f45d1eb199577614dcd5fc0db7ca5cc11
Tanya <tanya24k@gmail.com>
2022-12-10 16:58:23 +0300

    DP-2
    Nicknames

--- lessons/Program.cs
+++ lessons/Program.cs
@@ -6,17 +6,47 @@ public class Program
     private static List<Duck> _ducks = new List<Duck>();
     public static void Main(string[] args) 
     {
-        Console.WriteLine("Now this is Duck Park !!!");
+        Console.WriteLine("Now this is Duck Park !!! \r\nLet's take a look at our beautiful ducks:");
+       //Console.WriteLine("Let's take a look at our beautiful ducks: ");
         _ducks.Add(new Duck()
         {
-            nickname = "Bebe"
+            nickname = " Bebe"
         });
 
         _ducks.Add(new Duck()
         {
-            nickname = "Sergo"
+            nickname = " Sergo"
         });
 
-        Console.WriteLine("Count: {0}", _ducks.Count);
+         _ducks.Add(new Duck()
+        {
+            nickname = " Himdan ibn Mohammed II"
+        });
+
+        _ducks.Add(new Duck()
+        {
+            nickname = " Masha"
+        });
+        _ducks.Add(new Duck()
+        {
+            nickname = " Spartacus"
+        });
+
+        _ducks.Add(new Duck()
+        {
+            nickname = " Vova"
+        });
+
+        _ducks.Add(new Duck()
+        {
+            nickname = " Natasha"
+        });
+
+        foreach (var ducks in _ducks)
+        {
+            Console.WriteLine(ducks.nickname);
+        }
+
+        Console.WriteLine("Count of ducks: {0}", _ducks.Count);
     }
 }
