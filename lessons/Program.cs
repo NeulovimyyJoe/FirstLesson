816fbc8a1821c17001f9407cccad046c4f57ff22
Tanya <tanya24k@gmail.com>
2022-12-12 15:05:30 +0300

    DP-3.1
    Add a Duck

--- lessons/Program.cs
+++ lessons/Program.cs
@@ -4,11 +4,18 @@ using System.Collections.Generic;
 public class Program
 {
     private static List<Duck> _ducks = new List<Duck>();
-    public static void Main(string[] args) 
+   
+    public static void DuckEnterprise() 
+    { 
+        foreach (var duck in _ducks)
+        {
+            Console.Write(duck.nickname + ", ");
+        }
+    }
+   
+    public static void DuckGeneration()
     {
-        Console.WriteLine("Now this is Duck Park !!! \r\nLet's take a look at our beautiful ducks:");
-       
-        _ducks.Add(new Duck()
+         _ducks.Add(new Duck()
         {
             nickname = "Bebe"
         });
@@ -41,12 +48,36 @@ public class Program
         {
             nickname = "Natasha"
         });
+    }
+    
+    public static void Main(string[] args) 
+    {
+        SuggestMeADuck();
+            
+        Console.WriteLine("Now this is Duck Park !!! \r\nLet's take a look at our beautiful ducks:");
 
-        foreach (var duck in _ducks)
-        {
-            Console.Write(duck.nickname + ", ");
-        }
+        DuckGeneration();
+
+        DuckEnterprise();
 
         Console.WriteLine("\nCount of ducks: {0}", _ducks.Count);
     }
+    
+    public static void SuggestMeADuck()
+    {
+        Console.WriteLine("Would you like to add a duck? Write in the console Yes or No please.");
+            string answer1;
+            answer1 = Console.ReadLine();
+            switch (answer1)
+            {
+                case "Yes": Console.WriteLine("Give it a name");
+                    _ducks.Add(new Duck()
+                    {
+                        nickname = Console.ReadLine()
+                    });
+                    break;
+                case "No": Console.WriteLine("Welcome dear Guest!");
+                    break;
+                    default: goto case "No";
+    }       }   
 }
