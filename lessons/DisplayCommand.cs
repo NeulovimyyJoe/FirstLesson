53fc679f0fae61dddc131bb88e26bef8dc7b2155
Vladimir <116727687+NeulovimyyJoe@users.noreply.github.com>
2022-12-31 11:28:48 +0300

    DP BUG-1

--- lessons/DisplayCommand.cs
+++ lessons/DisplayCommand.cs
@@ -4,11 +4,15 @@ public class DisplayCommand : ICommand
 {
     public void Execute(string[] arguments)
     {
+        string displayLine = string.Empty;
         foreach (var duck in Program.ducks)
         {
-            Console.Write(duck.nickname + ", ");
+            displayLine += duck.nickname + ", ";
         }
 
+        displayLine = displayLine.Trim(new[] { ',', ' ' });
+        displayLine += ".";
+        Console.WriteLine(displayLine);
         Console.WriteLine("\nCount of ducks: {0}", Program.ducks.Count);
     }
 }
